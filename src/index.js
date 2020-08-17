22170bed87e8d012a1ddaf0e4e2b3013b8638180
Matheus Lima <matheus_sousa@protonmail.com>
2020-08-17 19:26:40 -0300

    app

---
+++ src/index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const { uuid}  = require('uuidv4');
+
+const app = express();
+
+app.use(express.json());
+
+const projects = [];
+
+app.get('/projects', (request, response) => {
+    
+    const {title} = request.query;
+
+    const results = title
+        ? projects.filter(project => project.filter.includes(title))
+        : projects;
+
+
+    return response.json(projects);
+});
+
+app.post('/projects', (request, response) => {
+    
+    const {title, owner} = request.body;
+    const project = {id: uuid(),title, owner};
+
+    projects.push(project)
+
+    return response.json(project)
+});
+
+app.put('/projects/:id', (request, response) => {
+    const { id, title, owner } = request.params;
+
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if(projectIndex < 0){
+        return response.status(400).json({error: 'project not found.'})
+    }
+
+    const project = {
+        id,
+        title,
+        owner
+    };
+
+    projects[projectIndex] = project;
+
+    return response.json(project)
+});
+
+app.delete('/projects/:id', (request, response) => {
+    const {id} = request.params;
+
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if(projectIndex < 0){
+        return response.status(400).json({error: 'project not found.'})
+    }
+
+    project.splice(projectIndex, 1);
+
+    return response.status(204).send();
+
+});
+
+app.listen(3333, () => {
+    console.log('Back-end started!');
+});
+
